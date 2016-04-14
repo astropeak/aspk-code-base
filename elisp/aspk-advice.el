372f754ffe50dbf9a093509a786ebed4f068f72c
Astropeak Luo <astropeak@gmail.com>
2016-04-14 21:20:13 +0800

    Add doc string. Fix some typo.

--- elisp/aspk-advice.el
+++ elisp/aspk-advice.el
@@ -1,15 +1,14 @@
 (defmacro aspk/advice-add (func-name pos action)
-  "Add a acvice `action' to `func-name' at `pos'. Currently `pos' can only be `before' and `after'."
+  "Add a advice `action' to `func-name' at `pos'. Currently `pos' can only be `before' and `after'."
   (message "Add advice. Function:%S, pos:%S, action:%S" func-name pos action)
   `(progn
      (defadvice ,func-name (,pos aspk-add-trace)
        (let ((args (ad-get-args 0)))
-         (apply ',action ',func-name ad-return-value args))
-       ;; (message "Return value: %S" ad-return-value)
-       )
+         (apply ',action ',func-name ad-return-value args)))
      (ad-activate ',func-name)))
 
 (defun aspk/advice-add-multi (func-name-list pos action)
+  "Add a advice `action' to `func-name-list' at `pos'. Currently `pos' can only be `before' and `after'."
   (dolist (func-name func-name-list)
     (eval `(aspk/advice-add ,func-name ,pos ,action))))
 
