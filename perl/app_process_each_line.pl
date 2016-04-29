2ac7c5dcae37e6e9d86fe01324c7fab20665822d
Astropeak Luo <astropeak@gmail.com>
2016-04-29 16:54:09 +0800

    add app_process_ecah_line.pl

---
+++ perl/app_process_each_line.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+# usage: app_process_each_line.pl COMMAND. COMMAND is valid perl statements. $_ is bound to the current line string.
+
+my $cmd=$ARGV[0];
+my $func = eval "sub{while(<STDIN>){chomp;$cmd;}}";
+die "unable to compile '$cmd', aborting...\n" if not defined $func;
+$func->();
+
