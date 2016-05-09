cb752b8fb775a97538541f3a1fcf84ea4e362a34
Astropeak Luo <astropeak@gmail.com>
2016-05-09 15:02:05 +0800

    add perl app set system time

---
+++ perl/app_set_system_time.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+# Set system time. Can only run under linux. Depended linux command: sudo, w3m, date.
+
+open my $fh, "w3m http://www.timeanddate.com/worldclock/china/chengdu -dump|" || die "Can't w3m";
+local $/;
+<$fh> =~ m/Home > Time Zones >(?:.*\n){3,8}([\d:]+)(?:.*\n){2}(.*)/;
+$cmd = "sudo date -s $1 $2";
+print "cmd: $cmd\n";
+system($cmd);
