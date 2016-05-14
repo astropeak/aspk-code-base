6347026588f1f8822bb239957ab9f22237cf80db
Astropeak Luo <astropeak@gmail.com>
2016-05-14 16:22:49 +0800

    fix 1. class object not printed good. 2. circular print

--- perl/Aspk/debug.pm
+++ perl/Aspk/debug.pm
@@ -1,24 +1,44 @@
 package Aspk::debug;
 use Exporter;
 
+use Scalar::Util qw(reftype);
+
 @ISA=qw(Exporter);
 @EXPORT_OK=qw(printHash);
 
 # first parameter is a references to a hash
+# TODO should be renamed to print_obj
+my $objs = {};
 sub printHash {
 	my $h = $_[0];
 	my $header = $_[1];
-	unless ($header) {$header="";};
+	unless ($header) {
+        print "Root: $h\n";
+        $header="    ";
+        $objs = {};
+    };
+    if (exists $objs->{$h}) {
+        print "$header Already printed\n";
+        return;
+    }
 
-	if (ref($h) eq 'HASH') {
+    $objs->{$h} = 1 if (reftype($h) eq 'HASH' || reftype($h) eq 'ARRAY');
+
+	if (reftype($h) eq 'HASH') {
         for my $k (keys %{$h}) {
             my $t = $h->{$k};
             print "$header $k: $t\n";
-            printHash($h->{$k}, $header."    ");
+            # if ($k ne "_parent"){ #TODO: this is only a workaround for Tree.pm. should be removed.
+                printHash($h->{$k}, $header."    ");
+            # }
 		}
-    } elsif (ref($h) eq 'ARRAY'){
-        my $t = join( ", " , @{$h});
-		print "$header $t\n";
+    } elsif (reftype($h) eq 'ARRAY'){
+        # my $t = join( ", " , @{$h});
+		# print "$header $t\n";
+        for(my $i=0; $i<scalar(@{$h});$i++) {
+            print($header." [$i]:".$h->[$i]."\n");
+            printHash($h->[$i], $header."    ");
+        }
     }
     else {
         # print "$header $h\n";
--- perl/Aspk/debug_example.pl
+++ perl/Aspk/debug_example.pl
@@ -3,6 +3,7 @@ use Aspk::debug qw(printHash);
 my %h = ("name"=>"Tom", "gender"=>"male");
 my $h1 = {"aa"=>"bb",
           "cc"=>"dd",
-"AA"=>\%h,
-"BB"=>[1,2,3,4]};
+          "AA"=>\%h,
+          "BB"=>[1,2,\%h,4]};
+
 printHash($h1);
