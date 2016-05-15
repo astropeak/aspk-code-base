bf15f837a4946ff17a7425889d6ae9b212dcdeaf
Astropeak Luo <astropeak@gmail.com>
2016-05-15 08:45:24 +0800

    add level debug print

--- perl/Aspk/Debug.pm
+++ perl/Aspk/Debug.pm
@@ -1,4 +1,5 @@
 package Aspk::Debug;
+use Filter::Simple;
 use Exporter;
 
 use Scalar::Util qw(reftype);
@@ -29,7 +30,7 @@ sub print_obj {
             my $t = $h->{$k};
             print "$header $k: $t\n";
             # if ($k ne "_parent"){ #TODO: this is only a workaround for Tree.pm. should be removed.
-                print_obj($h->{$k}, $header."    ");
+            print_obj($h->{$k}, $header."    ");
             # }
 		}
     } elsif (reftype($h) eq 'ARRAY'){
@@ -44,5 +45,82 @@ sub print_obj {
         # print "$header $h\n";
     }
 }
+
+my $dbg_current_level= 8;
+
+sub _aa {
+    my $str = shift;
+    # print "Input: $str\n";
+    my @a = grep {$_ ne ""} split /[, )(]/, $str;
+    # print_obj(\@a);
+    my $b = join(".', '.", map {"'$_='.".$_} @a);
+    $b.='."\n";';
+    # print $b."\n";
+    return $b
+
+}
+
+# split '$a $b $c', '($a,$b,$c)', '$a,$b,$c' to a list of '($a,$b,$c)'
+sub split_arg_list{
+    my $str = shift;
+    my @a = grep {$_ ne ""} split /[, )(]/, $str;
+    return @a;
+}
+
+
+FILTER {
+    # print "####### Enter FILTER: $_\n";
+    # print "####### Enter FILTER\n\n";
+    # print "seperator:".$\."\n";
+    my @all_lines = grep {$_->{content} ne ""} map {{linenum=>++$aaaa, content=>$_}}split /\n/, $_;
+    # print_obj \@all_lines;
+    # my $file_name = $0;
+    # my ($package, $filename, $line) = caller;
+
+    if ($dbg_current_level > 5) {
+        foreach (@all_lines) {
+            if ($_->{content} =~ /^\s*dbgd([^;]*)/) {
+                $_->{content} = "print '[', __PACKAGE__, '::', __SUB__, ':', __LINE__, ']', "."' '."._aa($1);
+            }
+        }
+
+        $_ = join("\n", map{$_->{content}} @all_lines);
+        $_ = "\nuse feature 'current_sub';\n".$_;
+    } else {
+        s/dbgd(.*)//g;
+    }
+
+
+    # print_obj \@all_lines;
+
+    # s/BANG\s+BANG/die 'die in filter: BANG' if 1/g;
+    # if ($dbg_current_level > 5) {
+    #     if (/dbg.([^;]*)/) {
+
+    #         # s/dbgd/print "$b\n";/g;
+
+    #     }
+    #     s/dbgd\s*(.*)/print '\1='._aa($1).'\n';/g;
+    # } else {
+    #     s/dbgd(.*)//;
+    # }
+
+    # print "####### Exit FILTER: $_\n";
+    # print "####### Exit FILTER\n";
+    # $_ = "print EEEEFFF\n';";
+};
+
+sub get_current_line {
+    my ($package, $filename, $line, $subroutine, $hasargs,
+        $wantarray, $evaltext, $is_require, $hints, $bitmask, $hinthash) = caller(0);
+    return $line;
+}
+sub get_parent_sub {
+    my ($package, $filename, $line, $subroutine, $hasargs,
+        $wantarray, $evaltext, $is_require, $hints, $bitmask, $hinthash) = caller(1);
+    return $subroutine;
+}
+
+
 
 1;
--- perl/Aspk/Debug_example.pl
+++ perl/Aspk/Debug_example.pl
@@ -1,9 +1,38 @@
 use Aspk::Debug qw(print_obj);
 
 my %h = ("name"=>"Tom", "gender"=>"male");
-my $h1 = {"aa"=>"bb",
+my $h1 = {"aa"=>"BB",
           "cc"=>"dd",
           "AA"=>\%h,
           "BB"=>[1,2,\%h,4]};
 
 print_obj($h1);
+
+
+
+
+sub aaa{
+    #  0         1          2      3            4
+    my ($package, $filename, $line, $subroutine, $hasargs,
+        #  5          6          7            8       9         10
+        $wantarray, $evaltext, $is_require, $hints, $bitmask, $hinthash) = caller(1);
+
+    print "aaa: ",$package, $filename, $line, $subroutine, $hasargs, "\n";
+    print_obj($hinthash);
+    print "subroutine: $subroutine\n";
+}
+sub bbb{
+    aaa(1,2,3);
+}
+sub ccc{
+    bbb(4,5,7);
+}
+ccc();
+
+# print __FILE__, ":", __LINE__, ":", __FUNCTION__, ": BBBB\n";
+
+# print __PACKAGE__, ": FFFF\n";
+
+my $str="AAAABBBB";
+my $num=33333;
+dbgd $str $num;
