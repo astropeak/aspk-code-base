9c304d4bdf63c808b2f283991da94073f7f34fb4
Astropeak Luo <astropeak@gmail.com>
2016-06-08 17:40:19 +0800

    add use mongoose.js. But the update function not work.

---
+++ nodejs/usage-mongoose.js
@@ -0,0 +1,38 @@
+// getting-started.js
+var mongoose = require('mongoose');
+var assert = require('assert');
+
+// mongoose.connect( 'mongodb://localhost:27017/myproject');
+mongoose.connect('mongodb://localhost/test');
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+    var kittySchema = mongoose.Schema({
+        name: String
+    });
+
+    var Kitten = mongoose.model('Kitten', kittySchema);
+    
+    var silence = new Kitten({ name: 'Silence' });
+    console.log(silence.name); // 'Silence'
+
+    // silence.save(function (err, silence) {
+    //     assert.equal(null, err);
+    //     console.log("saved to db. D: ", silence);
+    // });
+
+    Kitten.update({name:'hahah'}, {name:'aaaa', work:"bbbbb"},function(err, raw){
+
+        assert.equal(null, err);
+        console.log("update: raw: ", raw); 
+
+        Kitten.find({}, function (err, kittens) {
+            assert.equal(null, err);
+            console.log("all kittens: ", kittens);
+        });
+    });
+
+});
