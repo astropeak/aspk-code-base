ced23dd115bdd3e14361920325b2a15a4286018d
Astropeak Luo <astropeak@gmail.com>
2016-07-07 17:17:09 +0800

    some modification for mongoose usage

--- nodejs/usage-mongoose.js
+++ nodejs/usage-mongoose.js
@@ -11,28 +11,41 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     // we're connected!
     var kittySchema = mongoose.Schema({
-        name: String
+        name: String,
+        job:String
     });
 
     var Kitten = mongoose.model('Kitten', kittySchema);
-    
+
     var silence = new Kitten({ name: 'Silence' });
     console.log(silence.name); // 'Silence'
 
     // silence.save(function (err, silence) {
-    //     assert.equal(null, err);
-    //     console.log("saved to db. D: ", silence);
+    // assert.equal(null, err);
+    // console.log("saved to db. D: ", silence);
     // });
 
-    Kitten.update({name:'hahah'}, {name:'aaaa', work:"bbbbb"},function(err, raw){
+    Kitten.update({name:'aaaa'}, {name:'aaaa', job:"bbbbb"},function(err, raw){
 
         assert.equal(null, err);
-        console.log("update: raw: ", raw); 
+        console.log("update: raw: ", raw);
+
 
         Kitten.find({}, function (err, kittens) {
             assert.equal(null, err);
             console.log("all kittens: ", kittens);
         });
     });
+
+    // Kitten.findOne({ name: 'Silence' }, function (err, doc) {
+    //     assert.equal(null, err);
+    //     doc.name = 'jason borne';
+    //     console.log("find one.");
+    //     doc.job="Student";
+    //     doc.save(function(err, a){
+    //         assert.equal(null, err);
+    //         console.log("Saved. a: ", a);
+    //     });
+    // });
 
 });
