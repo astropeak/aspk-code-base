b8c5bcbc1b7d9b800eeac1921f031bbb1d9261f6
Astropeak <astropeak@gmail.com>
2018-02-28 16:07:46 +0800

    Add how to calculate gradients and usage of an optimizer

--- python/aspk/tensorflow_test.py
+++ python/aspk/tensorflow_test.py
@@ -33,8 +33,6 @@ class TensorflowTest(unittest.TestCase):
     self.assertEqual(r, 9)
 
 
-
-
   def test_variable_assign(self):
     a = tf.Variable(3, name='my_a')
     # a variable must be initilized
@@ -47,6 +45,89 @@ class TensorflowTest(unittest.TestCase):
     self.assertEqual(self.sess.run(a), 3)
     r = self.sess.run(assign_to_4)
     self.assertEqual(self.sess.run(a), 4)
+
+  def test_graident_calculation(self):
+    '''
+    Calculate a function's graident by tf.gradients function.
+
+    Refs:
+      - offical document: https://www.tensorflow.org/api_docs/python/tf/gradients
+    '''
+    var = tf.Variable(4)
+    loss = 2*var*var + 3*var + 4
+    def grad(x):
+      '''Calculate the above funcion's gradient'''
+      return 4*x + 3
+
+    # calculate Dloss / Dvar. This will add a node to the graph.
+    # gradients() adds ops to the graph to output the derivatives of ys with respect to xs. It returns a list of Tensor of length len(xs) where each tensor is the sum(dy/dx) for y in ys.
+    var_grad = tf.gradients(loss, [var])[0]
+
+    init = tf.global_variables_initializer()
+    self.sess.run(init)
+    var_grad_val = self.sess.run(var_grad)
+    var_val = self.sess.run(var)
+    # var_grad_val = 19
+    self.assertEqual(var_grad_val, grad(var_val))
+
+  def test_optimizer_two_step(self):
+    '''
+    Test usage of compute_gradients and apply_gradients.
+
+    Refs:
+      - https://www.tensorflow.org/api_docs/python/tf/train/GradientDescentOptimizer
+    '''
+    learning_rate = 0.01
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    var = tf.Variable(4.0)
+    loss = 2*var*var + 3*var + 4
+
+    # this call tf.graidents() internally
+    # the second parameter is the variable list. Return value is a list of (grad, var). Which can be passed to apply_gradients
+    grads_and_vars = optimizer.compute_gradients(loss, [var])
+    update_var = optimizer.apply_gradients(grads_and_vars)
+
+    init = tf.global_variables_initializer()
+    self.sess.run(init)
+    a = self.sess.run(grads_and_vars)
+    self.assertEqual(a, [(19, 4)])
+
+    v = self.sess.run(var)
+    self.assertEqual(v, 4)
+    b = self.sess.run(update_var)
+
+    v = self.sess.run(var)
+    # now v is 3.809999
+    self.assertTrue(v < 4)
+
+
+  def test_optimizer_minimize(self):
+    '''
+    The optimizer.minimize() operation will run one step to update the variable.
+    It just call compute_gradients and apply_gradients. All variables will be updated after one run.
+    The minimize() operation will also add a node to the graph. But the node is used only for side effect, that is updateing the variables.
+
+    Summary for an optimizer:
+    Accept a loss function and var list, then do one iteration step to find the optimal variable values then minimize the loss function.
+
+    Refs:
+      - https://www.tensorflow.org/api_docs/python/tf/train/GradientDescentOptimizer
+    '''
+    learning_rate = 0.01
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    var = tf.Variable(4.0)
+    loss = 2*var*var + 3*var + 4
+
+    # accept one optional parameter 'var_list'. If no provided, will get by GraphKeys.TRAINABLE_VARIABLES
+    minimizer = optimizer.minimize(loss)
+
+    init = tf.global_variables_initializer()
+    self.sess.run(init)
+    a = self.sess.run(minimizer)
+    v = self.sess.run(var)
+    # now v is 3.809999
+    self.assertTrue(v < 4)
+
 
 if __name__ == '__main__':
   suite = unittest.TestLoader().loadTestsFromTestCase(TensorflowTest)
