11f563be79c9fac18a929b23486e811d5960b54e
Astropeak <astropeak@gmail.com>
2019-10-01 16:52:01 +0800

    Make it works for yesno example. Now transcription could be generated given sound wav file

--- kaldi/sound_to_lattice.sh
+++ kaldi/sound_to_lattice.sh
@@ -23,23 +23,36 @@ model=xxx
 # for yesno eg
 graphdir=~/github/kaldi/egs/yesno/s5/exp/mono0a/graph_tgpr
 model=/Users/astropeak/github/kaldi/egs/yesno/s5/exp/mono0a/final.mdl
-sdata=/Users/astropeak/github/kaldi/egs/yesno/s5/data/test_yesno
+# sdata=/Users/astropeak/github/kaldi/egs/yesno/s5/data/test_yesno
+# directory
+sdata=data
 
 cmvn_opts=
 delta_opts=
 
+tmpdir=./tmp
+
+
+# 0 prepare wav.scp, utt2spk
+# 需要给出一个ID, 这个ID 在wav.scp 及utt2spk文件中会用.
+ID=wav_ID
+echo "$ID $sound_file" > $tmpdir/wav.scp
+echo "$ID global" > $tmpdir/utt2spk
+
+
 # 1: from sound to feature
 # 1.1 create mffc, stored in fests.scp
+compute-mfcc-feats  --use-energy=false --sample-frequency=8000 --verbose=2 scp:$tmpdir/wav.scp ark,scp:$tmpdir/feats.ark,$tmpdir/feats.scp
 
 # 1.2 process mffc, apply cmnv, add deltas
-feats="ark,s,cs:apply-cmvn $cmvn_opts --utt2spk=ark:$sdata/utt2spk scp:$sdata/cmvn.scp scp:$sdata/feats.scp ark:- | add-deltas $delta_opts ark:- ark:- |"
+feats="ark,s,cs:apply-cmvn $cmvn_opts --utt2spk=ark:$tmpdir/utt2spk scp:$sdata/cmvn.scp scp:$tmpdir/feats.scp ark:- | add-deltas $delta_opts ark:- ark:- |"
 # if remove --utt2spk, there is error like.
 # WARNING (apply-cmvn[5.4.218~2-e6fe]:main():apply-cmvn.cc:112) No normalization statistics available for key 1_1_1_1_1_1_1_1, producing no output for this utterance
 # LOG (apply-cmvn[5.4.218~2-e6fe]:main():apply-cmvn.cc:162) Applied cepstral mean normalization to 0 utterances, errors on 29
 # so no output will be produced by apply-cmnv
 # the utt2spk map all utt to a global speaker, which is a key to query cmnv statictics from cmnv.scp. So if that key is not availble, then no cmnv info, so on ouput
 # so in decoer, we sould always map a utt to the global speker.
-# feats="ark,s,cs:apply-cmvn $cmvn_opts scp:$sdata/cmvn.scp scp:$sdata/feats.scp ark:- | add-deltas $delta_opts ark:- ark:- |"
+# feats="ark,s,cs:apply-cmvn $cmvn_opts scp:$sdata/cmvn.scp scp:$tmpdir/feats.scp ark:- | add-deltas $delta_opts ark:- ark:- |"
 
 # 2: from feature to lattice
 gmm-latgen-faster --max-active=$max_active --beam=$beam --lattice-beam=$lattice_beam \
---
+++ kaldi/sound_to_transcription.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# give a sound wav file(should be 8K sample frequence), output the transcription
+# use some files of yesno, such as the model, word.txt, cmvn data, graph.
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 sound_file"
+    echo "  eg. $0 ./data/00001111.wav"
+    exit 1
+fi
+
+tmpdir=./tmp
+if [ ! -d $tmpdir ]; then
+    mkdir $tmpdir
+fi
+
+latfile=$tmpdir/lat.gz
+./sound_to_lattice.sh $1 $latfile 2>$tmpdir/log.log
+./lattice_to_transcription.sh $latfile ~/github/kaldi/egs/yesno/s5/exp/mono0a/graph_tgpr/words.txt 12 0.0 2>>$tmpdir/log.log
